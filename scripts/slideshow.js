9908e696cb6bcfaecaeedd31ea3a710f64684c41
Elithous <Elithous@gmail.com>
2018-03-09 11:12:09 -0700

    Better slide show transitions

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,18 +1,28 @@
-var image = $("#slideshow");
+var slideshow = $("#slideshow")
+var image = $("#nowSlide");
+
 var numOfImages = 4;
-var imageIndex = 0;
+var imageIndex = 1;
+
+var width = 400;
+var height = 300;
 
 function changeImage() {
-    image.fadeOut("slow", function() {
-        image.attr("src","images/slideshow/" + imageIndex + ".jpg");
-        image.fadeIn("slow");
+    slideshow.css({"background-image" : "url(images/slideshow/" + imageIndex + ".jpg)"});
+    image.fadeOut(700, function() {
+        image.attr("src", "images/slideshow/" + imageIndex + ".jpg");
+        image.css({"display" : "inherit"})
+        imageIndex++;
+        if (imageIndex >= numOfImages) {
+            imageIndex = 0;
+        }
     });
-    imageIndex++;
-    if (imageIndex >= numOfImages) {
-        imageIndex = 0;
-    }
-    console.log("changed");
+    
 }
 window.onload = function() {
+    slideshow.css({"width": width, "height": height, "background-size" : width + "px " + height + "px", "background-repeat" : "no-repeat"});
+    image.attr("width", width + "px");
+    image.attr("height", height + "px");
+    image.css({"position" : "absolute"})
     setInterval(changeImage,4000);
 } 

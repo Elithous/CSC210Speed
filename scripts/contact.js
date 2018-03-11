7b37f03471b3efea219470e57d21836613bf9f73
Paige <klima.paige@gmail.com>
2018-03-10 18:38:15 -0700

    Contact page can now enlarge the contact info when clicked and shrinks when clicked again

--- scripts/contact.js
+++ scripts/contact.js
@@ -17,7 +17,83 @@ function loadComplete(evt) {
     $("#address").append(map.results[0].address_components[1].long_name+" ");
     $("#city").html(map.results[0].address_components[3].long_name+", ");
     $("#city").append(map.results[0].address_components[5].long_name+" ");
-
-    
-
+    var aClicked = true;
+    $("#address").on("click", function(){
+        if(aClicked){
+            aClicked=!aClicked;
+            $("#address").css("font-size","2.5em");
+            cClicked=aClicked;
+            $("#city").css("font-size","2.5em");
+        } else{
+            aClicked=!aClicked;
+            $("#address").css("font-size","1em");
+            cClicked=aClicked;
+            $("#city").css("font-size","1em");
+        }
+    });
+    var cClicked = true;
+    $("#city").on("click", function(){
+        if(cClicked){
+            cClicked=!cClicked;
+            $("#city").css("font-size","2.5em");
+            aClicked=cClicked;
+            $("#address").css("font-size","2.5em");
+        } else{
+            cClicked=!cClicked;
+            $("#city").css("font-size","1em");
+            aClicked=cClicked;
+            $("#address").css("font-size","1em");
+        }
+    });
+    var pClicked = true;
+    $("#phone").on("click", function(){
+        if(pClicked){
+            pClicked=!pClicked;
+            $("#phone").css("font-size","2.5em");
+        } else{
+            pClicked=!pClicked;
+            $("#phone").css("font-size","1em");
+        }
+    });
+    var eClicked = true;
+    $("#email").on("click", function(){
+        if(eClicked){
+            eClicked=!eClicked;
+            $("#email").css("font-size","2.5em");
+        } else{
+            eClicked=!eClicked;
+            $("#email").css("font-size","1em");
+        }
+    });
+    var tClicked = true;
+    $("#twitter").on("click", function(){
+        if(tClicked){
+            tClicked=!tClicked;
+            $("#twitter").css("font-size","2.5em");
+        } else{
+            tClicked=!tClicked;
+            $("#twitter").css("font-size","1em");
+        }
+    });
+    var iClicked = true;
+    $("#instagram").on("click", function(){
+        if(iClicked){
+            iClicked=!iClicked;
+            $("#instagram").css("font-size","2.5em");
+        } else{
+            iClicked=!iClicked;
+            $("#instagram").css("font-size","1em");
+        }
+    });
+    var sClicked = true;
+    $("#snapchat").on("click", function(){
+        if(sClicked){
+            sClicked=!sClicked;
+            $("#snapchat").css("font-size","2.5em");
+        } else{
+            sClicked=!sClicked;
+            $("#snapchat").css("font-size","1em");
+        }
+    });
 }
+

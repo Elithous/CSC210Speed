455eee079f6e970c124fffe818fa316b360874b7
Paige <klima.paige@gmail.com>
2018-03-12 18:36:59 -0600

    Styling of all pages are done

--- scripts/about.js
+++ scripts/about.js
@@ -1,25 +1,28 @@
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
-var width = 300;
-var height = 300;
+var width = 320;
+var height = 400;
 var space = 30;
 var current = 60;
 context.font = "16px Arial";
-context.fillText("  0___________________________________", 10,315);
-context.fillText(" 10___________________________________", 10,315 - (10 *3));
-context.fillText(" 20___________________________________", 10,315 - (20* 3));
-context.fillText(" 30___________________________________", 10,315- (30 *3))
-context.fillText(" 40___________________________________", 10,315- (40 *3))
-context.fillText(" 50___________________________________", 10,315- (50 *3))
-context.fillText(" 60___________________________________", 10,315- (60 *3))
-context.fillText(" 70___________________________________", 10,315- (70 *3))
-context.fillText(" 80___________________________________", 10,315- (80 *3))
-context.fillText(" 90___________________________________", 10,315- (90 *3))
-context.fillText("100___________________________________", 10,315- (100 *3))
-context.fillText("Arrival",current -5,340);
-context.fillText("Safely", current + (space * 2) -5, 340);
-context.fillText("Accurate", current + (space * 4) -5, 340);
-context.fillText("Mortality", current + (space * 6 + 20) -5, 340);
+context.fillStyle = "#FFF";
+context.fillText("  0___________________________________", 10,335);
+context.fillText(" 10___________________________________", 10,335 - (10 *3));
+context.fillText(" 20___________________________________", 10,335 - (20* 3));
+context.fillText(" 30___________________________________", 10,335- (30 *3))
+context.fillText(" 40___________________________________", 10,335- (40 *3))
+context.fillText(" 50___________________________________", 10,335- (50 *3))
+context.fillText(" 60___________________________________", 10,335- (60 *3))
+context.fillText(" 70___________________________________", 10,335- (70 *3))
+context.fillText(" 80___________________________________", 10,335- (80 *3))
+context.fillText(" 90___________________________________", 10,335- (90 *3))
+context.fillText("100___________________________________", 10,335- (100 *3))
+context.fillText("              General Stats in Percentages", 10,325- (102 *3));
+context.fillText("Arrival",current -5,360);
+context.fillText("Safely", current + (space * 2+10) -5, 360);
+context.fillText("Accurate", current + (space * 4+10) -5, 360);
+context.fillText("Mortality", current + (space * 6 +22) -5, 360);
+
  drawChart();
  
  function drawSquare(height, width){
@@ -32,43 +35,43 @@ context.fillText("Mortality", current + (space * 6 + 20) -5, 340);
 
             context.beginPath()
             context.lineWidth="5";
-            context.strokeStyle="green";
-            context.moveTo(current,315);
-            context.lineTo(current, 318  - (97 * 3));
+            context.strokeStyle="#EE6C33";
+            context.moveTo(current,334);
+            context.lineTo(current, 339  - (97 * 3));
             
-            context.lineTo (current + space +5 ,318 - (97 * 3));
-            context.lineTo (current + space +5 ,315);
+            context.lineTo (current + space +5 ,339 - (97 * 3));
+            context.lineTo (current + space +5 ,334);
             
             context.stroke(); 
             context.closePath();
-            context.fillStyle="green";
+            context.fillStyle="#EE6C33";
             context.fill();
-            current = current + (space *2);
-            context.moveTo(current,315);
-            context.lineTo(current, 318  - (98 * 3));
+            current = current + (space *2+10);
+            context.moveTo(current,334);
+            context.lineTo(current, 339  - (98 * 3));
             
-            context.lineTo (current + space +5 ,318 - (98 * 3));
-            context.lineTo(current + space +5 ,315);
+            context.lineTo (current + space +5 ,339 - (98 * 3));
+            context.lineTo(current + space +5 ,334);
             
             context.stroke();
             context.closePath();
             context.fill();
-            current = current + (space *2);
-            context.moveTo(current,315);
-            context.lineTo(current, 319  - (99 * 3));
+            current = current + (space *2+10);
+            context.moveTo(current,335);
+            context.lineTo(current, 339  - (99 * 3));
             
-            context.lineTo (current + space +5 ,319 - (99 * 3));
-            context.lineTo(current + space +5 ,315);
+            context.lineTo (current + space +5 ,339 - (99 * 3));
+            context.lineTo(current + space +5 ,335);
             context.stroke();
             context.closePath();
             context.fill();
 
-            current = current + (space *2) +20;
-            context.moveTo(current,318);
-            context.lineTo(current, 318  - (3 * 3));
+            current = current + (space *2+10);
+            context.moveTo(current,337);
+            context.lineTo(current, 337  - (3 * 3));
             
-            context.lineTo (current + space +5 ,318 - (3 * 3));
-            context.lineTo(current + space +5 ,318);
+            context.lineTo (current + space +5 ,337 - (3 * 3));
+            context.lineTo(current + space +5 ,337);
             context.stroke();
             context.closePath();
             context.fill();
